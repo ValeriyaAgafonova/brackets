eb0415b03c28eae499ff3ddf66b97a05b805d4b5
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2021-08-30 17:47:27 +0300

    fix: solut

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) {
+    return false;
+  }
+
+  let array = str.split("");
+
+  let open = ['(', '[', '{','|', '1', '3','5','7','8'];
+  let pairs = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+    ['|']: '|',
+    ['1']: '2',
+    ['3']: '4',
+    ['5']: '6',
+    ['7']: '7',
+    ['8']: '8',
+  };
+  let stack = [];
+for (let i = 0; i < str.length; i++){
+  let current = str[i];
+  if (open.includes(current)){
+    stack.push(current);
+  } else {
+    if (stack.length === 0){
+     return false;
+    }
+    let top = stack[stack.length - 1];
+
+    if (pairs[current] === top){
+      stack.pop();
+    }
+    else{
+      return false;
+    }
+  }
+}
+return stack.length === 0;
 }
